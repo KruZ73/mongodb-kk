07c950ab8e1b4a9bf5111897e9754cd08e3c57a7
kruz <kruz@cos>
2023-10-16 12:59:59 +0200

    nic nie działa - baza danych do ogarniecia

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const mongoString = "mongodb+srv://admin:w7ArSUVAh2HXjM6R@cluster0.qtyrl5o.mongodb.net/?retryWrites=true&w=majority";
+const mongoString = "";
 
 async function main() {
     //stworz nowe połaczenie do bazy danych pod nazwa client używając sterownika MongoClient (alias do mongodb)
